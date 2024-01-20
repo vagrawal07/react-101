d2eb9e6c57a8d39a8a9c8129ca1b68f3b049ce0f
Vaibhav Agrawal <vaibhavagrawal55@gmail.com>
2024-01-20 12:25:47 -0500

    added packages and imports

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading1" }, "Hello World From React"),
+    React.createElement("h1", { id: "heading1" }, "Hello World From React "),
     React.createElement("h2", { id: "heading" }, "Hello World From React"),
   ]),
   React.createElement("div", { id: "child2" }, [
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # First React Code 
+
+
+# Parcel
+ - Dev Build
+ - Local Server
+ - HMR = Hot Module Replacement
+ - File Watching Algorithm - written in C++
+ - Cache - Faster Builds
+ - Image Optimization
+ - Minification
+ - Bundling
+ - Compressing
+ - Consistent Hashing
+ - Code Splitting
+ - Differential Bundling - support older browsers
+ - Diagnostic
+ - Error Handling
+ - Https
+ - Tree Shaking - remove unused code
+ - Different dev and prod bundles
