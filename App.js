2fef5dc6ada69e8c894ad9e2d66f6a32252c8cd9
Vaibhav Agrawal <vaibhavagrawal55@gmail.com>
2024-01-20 09:51:29 -0500

    first episode

---
+++ App.js
@@ -0,0 +1,14 @@
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading1" }, "Hello World From React"),
+    React.createElement("h2", { id: "heading" }, "Hello World From React"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading1" }, "Hello World From React"),
+    React.createElement("h2", { id: "heading" }, "Hello World From React"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
